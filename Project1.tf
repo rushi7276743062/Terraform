84f388cfe0fb25b89e119300ebebcefbb88664fc
mrrushikeshdaund <rushikesh.daund@yahoo.com>
2022-08-23 12:54:17 +0530

    adding some code

--- Project1.tf
+++ Project1.tf
@@ -56,3 +56,13 @@ data "aws_availability_zones" "available" {
     state = "available"
   
 }
+
+data "aws_ami" "aws-linux" {
+    most_recent = "true"
+    owners = ["amazon"]
+    filter {
+      name = "name"
+      values = ["amzn-ami-hvm*"]
+    }
+  
+}

275699c9f153b50a4e95a42fcb9dc54ffe59130f
mrrushikeshdaund <rushikesh.daund@yahoo.com>
2022-08-23 12:29:24 +0530

    add vpc code in project1.tf file

--- Project1.tf
+++ Project1.tf
@@ -36,3 +36,7 @@ provider "aws" {
 
 ################################################################################################################
 
+resource "aws_vpc" "myvpc" {
+  cidr_block =  = var.vpc_cidr
+  enable_dns_hostnames = "true"
+}

95d315364098794c0bf34a80b73f9e3e5adf0906
mrrushikeshdaund <rushikesh.daund@yahoo.com>
2022-08-23 12:23:43 +0530

    my first commit

---
+++ Project1.tf
@@ -0,0 +1,38 @@
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "ssh_key_name" {}
+
+variable "private_key_path" {}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+  
+}
+
+variable "public_subnet1_cidr" {
+  default = "10.0.0.0/24"
+}
+
+variable "private_subnet1_cidr" {
+  default = "10.0.1.0/24"
+}
+
+
+###############################################################################################################
+# Provider
+
+provider "aws" {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+    region = var.region
+}
+
+################################################################################################################
+

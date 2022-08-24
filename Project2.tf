82059db5751325ec34e817bf345a6ee5606fe601
mrrushikeshdaund <rushikesh.daund@yahoo.com>
2022-08-24 15:26:23 +0530

    adding some code

--- Project2.tf
+++ Project2.tf
@@ -1,6 +1,35 @@
 ###########################################################################################################
 # Variable
 
-variable "access_key" {
-  
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/0"
+}
+
+variable "public_subnet_cidr" {
+    default = "10.0.0.0/0"
+}
+
+variable "enviroment_list" {
+  type = list(string)
+  default = [ "Dev","QA","Stage","Production" ]
+
+}
+
+variable "enviroment_map" {
+  type = map(string)
+  default = {
+    "DEV" = "DEV",
+    "QA" = "QA",
+    "Stage" = "Stage",
+    "Production" = "production"
+  }
+
 }
